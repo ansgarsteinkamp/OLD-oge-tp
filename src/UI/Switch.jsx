318146103ca35e57f0a857acf0d745d2a347d78c
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2023-01-29 19:53:04 +0100

    First Commit

---
+++ src/UI/Switch.jsx
@@ -0,0 +1,27 @@
+import { Switch } from "@headlessui/react";
+
+function classNames(...classes) {
+   return classes.filter(Boolean).join(" ");
+}
+
+export default function MySwitch({ allocation, setAllocation }) {
+   return (
+      <Switch
+         checked={allocation}
+         onChange={setAllocation}
+         className={classNames(
+            allocation ? "bg-stone-600" : "bg-gray-200",
+            "relative inline-flex h-4 w-7 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-stone-500 focus:ring-offset-2"
+         )}
+      >
+         <span className="sr-only">Use setting</span>
+         <span
+            aria-hidden="true"
+            className={classNames(
+               allocation ? "translate-x-3" : "translate-x-0",
+               "pointer-events-none inline-block h-3 w-3 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+            )}
+         />
+      </Switch>
+   );
+}
